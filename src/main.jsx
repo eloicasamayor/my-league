96297a60743851f407845f9f1bcdb7b958c9fcf6
eloicasamayor <37459666+eloicasamayor@users.noreply.github.com>
2022-12-03 20:54:55 +0100

    homepage for gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 
 import { Provider } from "react-redux";
 import { store } from "./store";
@@ -14,13 +14,13 @@ import { TeamPage } from "./pages/TeamPage";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route index element={<LeaguesPage />} />
           <Route path=":leagueUrlName" element={<LeaguePage />} />
           <Route path=":leagueUrlName/:teamName" element={<TeamPage />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
