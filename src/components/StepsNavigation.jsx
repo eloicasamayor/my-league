1213d905deee1494f36af321e064888205d3e15a
eloicasamayor <37459666+eloicasamayor@users.noreply.github.com>
2023-02-12 23:31:38 +0100

    tabs styles

--- src/components/StepsNavigation.jsx
+++ src/components/StepsNavigation.jsx
@@ -5,38 +5,28 @@ export function StepsNavigation({
   onSelectMatchings,
 }) {
   return (
-    <ol className="h-8 flex mx-2 my-2 items-center gap-1 justify-center">
+    <ol className="h-8 flex mx-2 my-2 items-center gap-1 justify-center sm:gap-6 lg:gap-20">
       <li
-        className={`flex items-center p-2 text-2xl cursor-pointer " + ${
+        className={`flex items-center p-0 text-2xl cursor-pointer " + ${
           selectedTab === 0 ? "text-violet-200" : "text-zinc-500"
         }
           `}
         onClick={() => setSelectedTab(0)}
       >
         {"Teams"}
-        <ArrowRight
-          svgClassName={
-            selectedTab === 0 ? "stroke-violet-200" : "stroke-zinc-500"
-          }
-        />
       </li>
       <li
-        className={`flex items-center gap-2 p-2 text-2xl cursor-pointer " + ${
+        className={`flex items-center p-0 text-2xl cursor-pointer " + ${
           selectedTab === 1 ? "text-violet-200" : "text-zinc-500"
         }
           `}
         onClick={() => setSelectedTab(1)}
       >
         Players
-        <ArrowRight
-          svgClassName={
-            selectedTab === 1 ? "stroke-violet-200" : "stroke-zinc-500"
-          }
-        />
       </li>
 
       <li
-        className={`flex items-center gap-2 p-2 text-2xl cursor-pointer font-medium " + ${
+        className={`flex items-center p-0 text-2xl cursor-pointer font-medium " + ${
           selectedTab === 2 ? "" : "text-zinc-500"
         }
           `}
@@ -46,14 +36,9 @@ export function StepsNavigation({
         }}
       >
         Dates
-        <ArrowRight
-          svgClassName={
-            selectedTab === 2 ? "stroke-violet-200" : "stroke-zinc-500"
-          }
-        />
       </li>
       <li
-        className={`flex items-center gap-2 p-2 text-2xl cursor-pointer " + ${
+        className={`flex items-center p-0 text-2xl cursor-pointer " + ${
           selectedTab === 3 ? "" : "text-zinc-500"
         }
           `}
@@ -63,11 +48,6 @@ export function StepsNavigation({
         }}
       >
         Matchings
-        <ArrowRight
-          svgClassName={
-            selectedTab === 3 ? "stroke-violet-200" : "stroke-zinc-500"
-          }
-        />
       </li>
     </ol>
   );
