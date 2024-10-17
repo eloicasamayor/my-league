404a73f261674ada81c2365caada62c91c310737
eloicasamayor <37459666+eloicasamayor@users.noreply.github.com>
2024-10-17 13:19:51 +0200

    evitar error al fer shuffle d'un calendari amb dies de descans

--- src/helpers/addDatesToMatchings.js
+++ src/helpers/addDatesToMatchings.js
@@ -29,6 +29,26 @@ export function addDatesToMatchings({
   });
   return matchingsWithDates;
 }
+
+/**
+ * @typedef {Object} Matchings
+ * @property {string[][]} matches
+ * @property {Date} date
+ */
+
+/**
+ * @param {Matchings[]} matchings
+ * @param {string[][]} teamsCopy
+ * @returns {Matchings[]}
+ */
 export function shuffleMatchings(matchings, teamsCopy) {
+  let ei = matchings.hola;
+  matchings.map((m, i) => {
+    // si en aquesta jornada no hi ha partits, en la mateixa posició de teamsCopy hi afegeixo un array buit.
+    if (!m.matches.length) {
+      teamsCopy.splice(i, 0, []);
+    }
+  });
+
   return matchings.map((m, i) => ({ ...m, matches: teamsCopy[i] }));
 }
