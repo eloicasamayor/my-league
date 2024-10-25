553ad808787e6e9ab138e7bd11040b3fc90d0ce9
eloicasamayor <37459666+eloicasamayor@users.noreply.github.com>
2024-10-25 08:34:55 +0200

    moure botó all/my leagues a dalt de la taula de lligues, per millor responsive

--- src/components/LeaguesList.jsx
+++ src/components/LeaguesList.jsx
@@ -29,63 +29,65 @@ export function LeaguesList({ authData, leaguesData, leaguesIsLoading }) {
   }
 
   return (
-    <Table hoverable={true}>
-      <Table.Head>
-        <Table.HeadCell className="text-base"></Table.HeadCell>
-        <Table.HeadCell className="text-base flex justify-between items-center">
-          name
-          {authData?.user?.id && (
-            <Button.Group>
-              <Button
-                color={isMyLeagues ? "gray" : "info"}
-                onClick={() => setIsMyLeagues(false)}
-              >
-                All
-              </Button>
-              <Button
-                color={!isMyLeagues ? "gray" : "info"}
-                onClick={() => setIsMyLeagues(true)}
-              >
-                My leagues
-              </Button>
-            </Button.Group>
-          )}
-        </Table.HeadCell>
-        <Table.HeadCell className="text-base">Description</Table.HeadCell>
-      </Table.Head>
-      <Table.Body>
-        {filteredLeaguesData?.map((league) => (
-          <Table.Row
-            className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 cursor-pointer"
-            key={league.id}
-            onClick={() => navigate(league.urlname)}
+    <>
+      {authData?.user?.id && (
+        <Button.Group>
+          <Button
+            color={isMyLeagues ? "gray" : "info"}
+            onClick={() => setIsMyLeagues(false)}
           >
-            <Table.Cell className="px-1 md:px-6 py-2 md:py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-              {league.img ? (
-                <img
-                  src={league.img}
-                  className={
-                    "inline-block h-10 w-10 rounded-xl ring-2 ring-white"
-                  }
-                ></img>
-              ) : (
-                <div className="inline-block h-10 w-10 rounded-xl ring-2 ring-white bg-violet-100 p-1">
-                  <PhotoIcon
-                    svgClassName={"stroke-violeta-600"}
-                    pathClassName={"stroke-violet-400"}
-                  />
-                </div>
-              )}
-            </Table.Cell>
-            <Table.Cell className="px-1 md:px-6 py-2 md:py-4 text-base">
-              {league.name}
-            </Table.Cell>
-            <Table.Cell className="px-1 md:px-6 py-2 md:py-4 text-base">
-              {league.description}
-            </Table.Cell>
-          </Table.Row>
-        ))}
-      </Table.Body>
-    </Table>
+            All
+          </Button>
+          <Button
+            color={!isMyLeagues ? "gray" : "info"}
+            onClick={() => setIsMyLeagues(true)}
+          >
+            My leagues
+          </Button>
+        </Button.Group>
+      )}
+      <Table hoverable={true}>
+        <Table.Head>
+          <Table.HeadCell className="text-base"></Table.HeadCell>
+          <Table.HeadCell className="text-base flex justify-between items-center">
+            name
+          </Table.HeadCell>
+          <Table.HeadCell className="text-base">Description</Table.HeadCell>
+        </Table.Head>
+        <Table.Body>
+          {filteredLeaguesData?.map((league) => (
+            <Table.Row
+              className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 cursor-pointer"
+              key={league.id}
+              onClick={() => navigate(league.urlname)}
+            >
+              <Table.Cell className="px-1 md:px-6 py-2 md:py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                {league.img ? (
+                  <img
+                    src={league.img}
+                    className={
+                      "inline-block h-10 w-10 rounded-xl ring-2 ring-white"
+                    }
+                  ></img>
+                ) : (
+                  <div className="inline-block h-10 w-10 rounded-xl ring-2 ring-white bg-violet-100 p-1">
+                    <PhotoIcon
+                      svgClassName={"stroke-violeta-600"}
+                      pathClassName={"stroke-violet-400"}
+                    />
+                  </div>
+                )}
+              </Table.Cell>
+              <Table.Cell className="px-1 md:px-6 py-2 md:py-4 text-base">
+                {league.name}
+              </Table.Cell>
+              <Table.Cell className="px-1 md:px-6 py-2 md:py-4 text-base">
+                {league.description}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+    </>
   );
 }
